108c855eea4634b5e4fec644d8f481e43f429117
BradJones-3 <bradjones_95@hotmail.com>
2021-08-18 12:36:12 +0000

    Calculate stock numbers and add them to the stock worksheet

--- run.py
+++ run.py
@@ -14,11 +14,13 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
     Get Sales Figures Input From User.
     Run a while loop to collect a valid string of data from the user
-    via the terminal, which has to be a string of 6 numbers seperated by commas. The loop will repeat until data is valid.
+    via the terminal, which has to be a string of 6 numbers seperated by commas.
+    The loop will repeat until data is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
@@ -66,6 +68,7 @@ def update_worksheet(data, worksheet):
     worksheet_to_update.append_row(data)
     print(f"{worksheet} worksheet updated successfully...\n")
 
+
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate surplus for each item type
@@ -85,8 +88,6 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
-
-
 def get_last_5_entries_sales():
     """
     Collects columns of data from sales worksheet, collecting
@@ -101,7 +102,20 @@ def get_last_5_entries_sales():
     
     return columns
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
 
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    
+    return new_stock_data
 
 def main():
     """
@@ -112,7 +126,9 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 
 main()
-sales_columns = get_last_5_entries_sales()
